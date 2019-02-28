a69114ace6adcadff9607ec73a8fb754bb669975
Mark <mark_otte@live.co.uk>
2019-02-28 16:20:05 +0000

    Added Scripts to button...
    Improved UI,  structured the directory

---
+++ CollabDev/Assets/Scripts/OptionsHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsHandler : MonoBehaviour
+{
+    public GameObject m_options;
+    public GameObject m_main;
+
+    bool m_isMainMenu = true;
+
+    public void OnOptionsClicked()
+    {
+        m_isMainMenu = false;
+
+        m_options.SetActive(true);
+        m_main.SetActive(false);
+    }
+
+    public void OnMenuClicked()
+    {
+        m_isMainMenu = true;
+
+        m_options.SetActive(false);
+        m_main.SetActive(true);
+    }
+}
