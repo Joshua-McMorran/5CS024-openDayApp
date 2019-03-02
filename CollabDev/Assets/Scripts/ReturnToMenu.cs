d8e7f65d711c829146f9c17b78591abd2b4b36a3
Mark <mark_otte@live.co.uk>
2019-03-02 18:19:21 +0000

    Added Camera
    Updated navigation

---
+++ CollabDev/Assets/Scripts/ReturnToMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    public void goBack()
+    {
+        SceneManager.LoadScene("MenuPage");
+    }
+
+    public void QR()
+    {
+        SceneManager.LoadScene("QR-Reader");
+    }
+}
