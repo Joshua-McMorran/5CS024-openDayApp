5241d7e246f70899fac85d14e7cd6ea2f9817b01
Mark <mark_otte@live.co.uk>
2019-03-27 15:46:21 +0000

    Ready for database
    Added script to enable database connection

---
+++ CollabDev/Assets/Scripts/PD_DBConnect.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PD_DBConnect : MonoBehaviour
+{
+
+    public TMPro.TMP_InputField F_Name;
+    public TMPro.TMP_InputField S_Name;
+    public TMPro.TMP_InputField Email;
+    public TMPro.TMP_InputField Phone_No;
+    public TMPro.TMP_InputField Post_Code;
+    public Toggle Phone;
+    public Toggle Email_Toggle;
+    public Toggle No_Contact;
+
+    public void Submit()
+    {
+        Debug.Log(F_Name.text);
+        Debug.Log(S_Name.text);
+        Debug.Log(Email.text);
+        Debug.Log(Phone_No.text);
+        Debug.Log(Post_Code.text);
+
+
+        if (Phone.isOn)
+        {
+            Debug.Log("Wants to be Texted");
+        } else
+        {
+            Debug.Log("Doesn't want to be Texted");
+        }
+
+        if (Email_Toggle.isOn)
+        {
+            Debug.Log("Wants to be Emailed");
+        } else
+        {
+            Debug.Log("Doesn't want to be Emailed");
+        }
+
+        if (No_Contact.isOn)
+        {
+            Debug.Log("Doesn't want to be contacted");
+        } else
+        {
+            Debug.Log("Wants to be contacted");
+        }
+    }
+}
